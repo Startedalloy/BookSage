f0b8e08638867452c0c60247d02bc2eb436ec4ff
Kamran Zaheer <kamranzaheer345@gmail.com>
2026-09-23 18:29:18 +0500

    fixed duplicate uri error

--- app/src/main/java/com/example/booksage/roomDB/Pdf.kt
+++ app/src/main/java/com/example/booksage/roomDB/Pdf.kt
@@ -2,13 +2,14 @@ package com.example.booksage.roomDB
 
 import android.net.Uri
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
 
-@Entity
+@Entity(indices = [Index(value = ["uri"], unique = true)])
 data class Pdf(
     @PrimaryKey(autoGenerate = true)
-    val id : Int = 0,
+    val id: Int = 0,
 
     val uri: Uri
 
--- app/src/main/java/com/example/booksage/roomDB/PdfDao.kt
+++ app/src/main/java/com/example/booksage/roomDB/PdfDao.kt
@@ -3,13 +3,14 @@ package com.example.booksage.roomDB
 import android.net.Uri
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PdfDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUri(pdf: Pdf)
 
     @Query("Select * From Pdf")
